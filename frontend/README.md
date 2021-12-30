0332859b89aa29cb1dfed3601a429119217a71ab
ICheered <tjeerd992@gmail.com>
2021-12-30 19:59:17 +0100

    First commit

---
+++ frontend/README.md
@@ -0,0 +1,11 @@
+# ICheered portfolio/show-off website
+This project is made using the EmberJS and TailwindCSS frameworks.
+
+## Running 
+Make sure NodeJS and EmberCLI are installed. Run the following commands in seperate terminals
+
+```console
+$ npm run tailwind // Will start the tailwind JIT compiler
+$ npm start // Wil start EmberJS liveview
+```
+The webpage is now visible at `http://localhost:4200`.
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [`./app/**/*.{js,ts,hbs}`],
+  theme: {
+    extend: {
+      colors: {
+        bgwhite: '#F1F1F1',
+      },
+      fontFamily: {
+        body: ['Manrope'],
+      },
+      transitionProperty: {
+        height: 'height'
+      },
+      keyframes: {
+        slide: {
+          from: {top: '-18rem'},
+          to: {top: '0rem'}
+        }
+      },
+      animation: {
+        slide: 'slide 3s ease-in-out',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      visibility: ["group-hover", "opacity"],
+    },
+   },
+  plugins: [],
+}
